f74802537fe3a56992197fce9621edb71ec41ac9
SuvuYou <nazarsuvyliak1@gmail.com>
2025-09-15 11:13:46 +0300

    switch to android

--- Assets/Scripts/Systems/GameManager.cs
+++ Assets/Scripts/Systems/GameManager.cs
@@ -4,6 +4,11 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private GlobalEventsBusSO _globalEventsBus;
 
+    private void Start() 
+    {
+        Application.targetFrameRate = 60;
+    }
+
     public void RestartGame() 
     {
         _globalEventsBus.TriggerOnRestartGame();
